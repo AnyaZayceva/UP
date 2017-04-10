d5f572412fde1655de6ab408d738a63cbfc646e9
AnyaZajceva <kroszik@gmail.com>
2017-04-11 00:45:40 +0300

    add some request

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static('public'));
-app.get('/index', function (req, res) {
-    res.send('');
-});
-app.listen(3000, function () {
-    console.log('Yahooooooo!')
-});
